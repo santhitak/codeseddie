fe6c3ad1a609c6e607c772d264c8e8e0eb4c5a63
santhitak <santhita.sk@gmail.com>
2021-11-12 20:10:47 +0700

    del: remove old component

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,20 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
-  import './lib/css/fontface.css'
+  import './lib/css/fontface.css';
+  import Nav from './lib/base/nav.svelte';
 </script>
 
-<main>
-  <Counter />
-</main>
+
+<body>
+  <Nav />
+</body>
 
 <style>
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
-
-  h1 {
-    color: #001aff;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 400;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
+  body{
+    width: 100vw;
   }
 </style>
