32d38529f0fb5c14ab50aac387cc0effa58b2183
santhitak <santhita.sk@gmail.com>
2021-11-12 19:16:44 +0700

    feat: fontface setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,18 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import './lib/css/fontface.css'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
   <h1>Hello Typescript!</h1>
 
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style>
+
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'SQ Market', sans-serif;
   }
 
   main {
@@ -32,33 +21,18 @@
     margin: 0 auto;
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
   h1 {
-    color: #ff3e00;
+    color: #001aff;
     text-transform: uppercase;
     font-size: 4rem;
-    font-weight: 100;
+    font-weight: 400;
     line-height: 1.1;
     margin: 2rem auto;
     max-width: 14rem;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
   @media (min-width: 480px) {
     h1 {
-      max-width: none;
-    }
-
-    p {
       max-width: none;
     }
   }
