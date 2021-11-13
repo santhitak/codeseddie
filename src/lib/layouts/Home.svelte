a01bc4102f40738fa6da911c93e000433eba7615
santhitak <santhita.sk@gmail.com>
2021-11-13 22:24:41 +0700

    refactor: global style

--- src/lib/layouts/Home.svelte
+++ src/lib/layouts/Home.svelte
@@ -1,39 +1,23 @@
 <script lang="ts">
   import ModeToggle from "@components/ModeToggle.svelte";
   import NextButton from "@components/NextButton.svelte";
+  import Tabs from "@components/Tabs.svelte";
 </script>
 
 <section>
+  <Tabs />
   <ModeToggle />
   <div class="wrapper">
     <div class="container">
-      <h1>ten days of coding<NextButton /></h1>
+      <h1>ten days<br>of code<NextButton /></h1>
     </div>
   </div>
 </section>
-
 <style>
   h1{
     margin: 0;
     padding: 0;
     font-size: 8rem;
-  }
-  :global(section){
-    width: 100vw;
-    height: 100vh;
-  }
-  :global(.wrapper){
-    height: 100%;
-    width: 100%;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-  }
-  .container{
-    height: 75vh;
-    width: 60vw;
-    align-items: center;
-    display: flex;
-    justify-content: center;
+    user-select: none;
   }
 </style>
