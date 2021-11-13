0fcb9111dcca9b1bbd59ca1efefcd6ec7b1091ad
santhitak <santhita.sk@gmail.com>
2021-11-13 22:41:18 +0700

    feat: footer

---
+++ src/lib/layouts/Footer.svelte
@@ -0,0 +1,23 @@
+<section style="height: auto;">
+  <div class="container footer">
+    <div class="listWrap">
+      <p>made with curiousity</p>
+      <p>&copy; All rights reserved 2021</p>
+    </div>
+  </div>
+</section>
+
+<style>
+  p {
+    font-size: 0.8rem;
+  }
+  .footer {
+    height: 20vh;
+  }
+  .listWrap {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+</style>
