5f6dd9304f2ad90c19bbec2b6e5d2e9f5e907c32
santhitak <santhita.sk@gmail.com>
2021-11-13 16:32:31 +0700

    config: url

---
+++ src/lib/layouts/Layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+  <h1>Hi</h1>
+</div>
+
+<style>
+  h1{
+    margin: 0;
+    padding: 0;
+  }
+
+  .container{
+    width: 100vw;
+    height: 100vh;
+    background-color: blueviolet;
+  }
+</style>
