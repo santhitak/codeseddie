93a038cab9cc2c22234b975163262066bbe4dccc
santhitak <santhita.sk@gmail.com>
2021-11-13 21:14:54 +0700

    feat: git link

---
+++ src/lib/components/Tabs.svelte
@@ -0,0 +1,20 @@
+<div class="Wrapper">
+  <a href="https://github.com/santhitak" target="_blank">Github</a>
+</div>
+
+<style>
+  a {
+    text-decoration: none;
+    color: var(--dark);
+    font-weight: 500;
+  }
+  :global(body.dark-mode) a {
+    color: var(--gray);
+  }
+  .Wrapper{
+    position: fixed;
+    top: 2.5rem;
+    left: 3rem;
+  }
+
+</style>
