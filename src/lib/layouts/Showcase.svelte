e2e68f88d597f7f6706ebeb0d58da9e95c17fee2
santhitak <santhita.sk@gmail.com>
2021-12-08 04:42:21 +0700

    add: showcase

--- src/lib/layouts/Showcase.svelte
+++ src/lib/layouts/Showcase.svelte
@@ -1,7 +1,95 @@
 <section>
+    <div class="container">
+        <div class="wrapper">
+            <div class="card">
+                <div class="content-wrapper">
+                    <div class="card-content">
 
+                    </div>
+                    <div class="proj-preview">
+                        <div class="prod-wrapper">
+                            <div class="prod-name">
+
+                            </div>
+                            <div class="prod-img">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content-wrapper">
+                    <div class="card-content">
+
+                    </div>
+                    <div class="proj-preview">
+                        <div class="prod-wrapper">
+                            <div class="prod-name">
+
+                            </div>
+                            <div class="prod-img">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content-wrapper">
+                    <div class="card-content">
+
+                    </div>
+                    <div class="proj-preview">
+                        <div class="prod-wrapper">
+                            <div class="prod-name">
+
+                            </div>
+                            <div class="prod-img">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content-wrapper">
+                    <div class="card-content">
+
+                    </div>
+                    <div class="proj-preview">
+                        <div class="prod-wrapper">
+                            <div class="prod-name">
+
+                            </div>
+                            <div class="prod-img">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <style>
+.wrapper{
+    display: grid;
+    grid-template-columns: 45% 45%;
+    grid-template-rows: 45% 45%;
+    row-gap: 5%;
+    column-gap: 5%;
+    justify-items: center;
+    align-items: center;
+    grid-auto-flow: row;
+}
+
+.card{
+    height: 100%;
+    width: 100%;
+    background-color: var(--dark);
+    border-radius: 20px;
+}
 
 </style>
